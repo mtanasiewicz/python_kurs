1dccf8f40bcfc2cc14a761c81ace25d9875c4e2a
mtanasiewicz <mtanasiewicz@example.com>
2020-12-12 16:47:26 +0100

    data types

---
+++ code/basics/data_types.py
@@ -0,0 +1,16 @@
+# Wszystko jest obiektem. Do sprawdzenia jakie sa dostępne funkcje w obiektcie służy dir()
+string = 'string'
+print(dir(string))
+
+# aby sprawdzic typ zmiennej uzywamy type()
+print(type(string))
+
+float = 4.25
+print(dir(float))
+print(type(float))
+
+# booleany piszemy zawsze z dużej
+print(dir(True))
+print(type(True))
+print(dir(False))
+print(type(False))
