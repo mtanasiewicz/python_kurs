03136fb711655aa4ab10006cd9a8168d74a9e2ed
mtanasiewicz <mtanasiewicz@example.com>
2020-12-12 19:16:00 +0100

    self

--- code/OOP/self.py
+++ code/OOP/self.py
@@ -4,7 +4,10 @@ class Tree:
     height = 25
 
     def print_info(self):
-        print('Nazwa: {}, wiek: {}, wysokość: {}'.format(self.name, self.age, self.height))
+        print(f'Nazwa: {self.name}, wiek: {self.age}, wysokość: {self.height}')
 
-tree_1 = Tree();
-print(tree_1.print_info())
+tree_1 = Tree()
+info = tree_1.print_info()
+info2 = Tree.print_info(tree_1)
+
+print(info, info2)
