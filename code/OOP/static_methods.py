8b553e88e757e47b65c83b94fddd6166387c5a44
mtanasiewicz <mtanasiewicz@example.com>
2020-12-12 19:42:40 +0100

    static methods

---
+++ code/OOP/static_methods.py
@@ -0,0 +1,16 @@
+class Student:
+    students_list = []
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        self.students_list.append(self)
+
+    @staticmethod
+    def students_count():
+        print(len(Student.students_list))
+
+student_1 = Student('Marian', 15)
+student_2 = Student('Michał', 18)
+
+Student.students_count()
